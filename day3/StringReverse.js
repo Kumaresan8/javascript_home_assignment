9736f95eed1bd889694b20317df7c19f74dc3d84
Kumaresan <kumaresanrks88@gmail.com>
2025-07-27 10:54:30 +0530

    fix: new add

--- day3/StringReverse.js
+++ day3/StringReverse.js
@@ -6,10 +6,6 @@ reverseName+=companyName.charAt(i);
  console.log(reverseName);
 
  let name1='kumaresan ';
- let re=[];
- for(let i=name1.length-1;i>=0;i--){
-re+=name1.charAt(i);
- }
- console.log(name1);
+
  
  
